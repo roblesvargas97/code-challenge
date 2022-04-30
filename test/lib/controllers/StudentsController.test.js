902e1cc197f7600b8203698dad551fecb4c6ada9
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-29 19:38:28 -0500

    Cambiando nombre del StudentsController junto con sus pruebas

--- test/lib/controllers/StudentsController.test.js
+++ test/lib/controllers/StudentsController.test.js
@@ -1,14 +1,14 @@
-const StudentController = require("../../../lib/controllers/StudentController");
+const StudentsController = require("../../../lib/controllers/StudentsController");
 
 describe("Test for StudentController", () => {
 
     test("1) getStudent should Return something ", () => {
-        const students = StudentController.getStudents();
+        const students = StudentsController.getStudents();
         expect(students.length).not.toEqual(0);
     });
 
     test("2)getStudents shouldnt return The file does not exists ", ()=>{
-        const students = StudentController.getStudents();
+        const students = StudentsController.getStudents();
         expect(students).not.toBe("The file does not exists.");
     });
 
